b155b6c5a6125a234de3e8ead96b3849db14ac9e
Marks <maxidrom310@gmail.com>
2022-12-14 03:56:58 +0300

    3.1

--- 3.1/prog.py
+++ 3.1/prog.py
@@ -4,25 +4,30 @@ def classes(time_string):
         print("Today is sunday")
         return
     cur_date = datetime.now().date()
+    
     hh, mm, ss =(int(s) for s in time_string.split(":"))
     t= datetime(cur_date.year, cur_date.month , cur_date.day , hh, mm, ss)
+    
     timetable = [datetime(cur_date.year, cur_date.month , cur_date.day,9,30, 0), 
     datetime(cur_date.year, cur_date.month , cur_date.day,11, 15, 0), 
     datetime(cur_date.year, cur_date.month , cur_date.day,13, 40, 0), 
     datetime(cur_date.year, cur_date.month , cur_date.day,15, 25, 0), 
     datetime(cur_date.year, cur_date.month , cur_date.day,17, 10, 0)]
-    i =-1
-    for u_class in timetable:
-        if t < u_class:
+    
+    current_class = None
+    for _class in timetable:
+        if t < _class:
             break
-        i+=1
-    if i == -1:
+        current_class = _class
+    
+    if current_class is None:
         print("Classes have not started yet")
         return
-    delta =t - timetable[i]
+    
+    delta =t - current_class
     class_length = timedelta(hours=1,minutes= 35,seconds= 0)
     if delta>class_length:
-        if i==4:
+        if current_class==timetable[-1]:
             print("Classes are over!")
             return
         print("Break time!")
@@ -31,4 +36,7 @@ def classes(time_string):
 
 #classes("15:15:01")
 #classes("7:15:01")
-classes(datetime.now().strftime("%H:%M:%S"))
+t = input("Time: ")
+if(len(t) == 0):
+    t = datetime.now().strftime("%H:%M:%S")
+classes(t)
